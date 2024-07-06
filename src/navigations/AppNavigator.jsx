3538c810154790fb3e03adeb98783f38301614e8
sitimaisaroh291001 <sitimaisaroh291001@gmail.com>
2024-07-06 10:33:48 +0700

    Menambah navigasi

---
+++ src/navigations/AppNavigator.jsx
@@ -0,0 +1,22 @@
+// src/navigation/AppNavigator.js
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import RecipeDetailScreen from '../screens/RecipeDetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
